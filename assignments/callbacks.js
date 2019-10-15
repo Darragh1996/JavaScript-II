0e25e631e5dc0f31a8785be8f3c873f643c7c526
Darragh Ferry <darraghferry@hotmail.com>
2019-10-15 12:34:11 +0100

    finshed challenges in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,24 +41,56 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  sum = x + y;
+  return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  product = x * y;
+  return cb(product);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)){
+    return cb(true);
+  }
+  return cb(false);
 }
+
+function print(val){
+  console.log(val);
+}
+
+function test(){
+  console.log("***getLength");
+  getLength(items, print);
+  console.log("***last");
+  last(items, print);
+  console.log("***sumNums");
+  sumNums(4, 5, print);
+  console.log("***multiplyNums");
+  multiplyNums(4, 5, print);
+  console.log("***contains - true");
+  contains('Pencil', items, print);
+  console.log("***contains - false");
+  contains('Shoe', items, print);
+
+}
+
+test();
 
 /* STRETCH PROBLEM */
 
